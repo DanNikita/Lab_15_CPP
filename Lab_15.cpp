425d1d5bff4177012f33de4d2803c13ee94e1f41
DanilovNV <1995dnv@gmail.com>
2024-08-02 10:31:10 +0300

    Add project files.

---
+++ Lab_15.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+struct StudentGrade {
+    string name;
+    char mark;
+};
+
+int main() {
+  map<string, char> grades;
+  int n = 10; //amount of students
+    for (int i =1; i <= n;i++)
+    {
+        cout << "Enter students name";
+        string name;
+        getline(cin, name);
+        cout << "Enter a mark for " << name << ": ";
+        char mark;
+        cin >> mark;
+        cin.ignore(); 
+        grades[name] = mark;
+        cout << "Stident:  " << name << " Mark: " << mark << ".\n";
+    }
+    cout << "Total marks:\n";
+    for (auto& pair : grades) 
+    {
+        cout << pair.first << ": " << pair.second << "\n";
+    }
+    return 0;
+}
+
